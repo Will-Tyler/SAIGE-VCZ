ac6ab9411dfaf49608a86176867d40a1d41e42d8
willtyler <will.tyler11@gmail.com>
2024-09-18 19:02:23 +0000

    Use pimpl idiom to hide tensorstore

--- src/VCZ.cpp
+++ src/VCZ.cpp
@@ -1,3 +1,5 @@
+#include "VCZ.hpp"
+
 #include <string>
 
 #include "tensorstore/array.h"
@@ -6,11 +8,25 @@
 #include "tensorstore/open.h"
 #include "tensorstore/spec.h"
 #include "tensorstore/tensorstore.h"
-#include "VCZ.hpp"
+
+using ::tensorstore::Index;
+using ::tensorstore::TensorStore;
 
 namespace VCZ {
 
-VczClass::VczClass(std::string t_vczFileName, std::vector<std::string> t_SampleInModel) {
+class VczClass::Impl {
+   public:
+    tensorstore::TensorStore<int8_t, tensorstore::dynamic_rank,
+                             tensorstore::ReadWriteMode::read>
+        m_store;
+    std::string m_chrom;
+    int m_pos;
+    int m_startPos;
+    int m_endPos;
+};
+
+VczClass::VczClass(std::string t_vczFileName,
+                   std::vector<std::string> t_SampleInModel) {
     std::string path = t_vczFileName + "/call_genotype";
     tensorstore::Spec input_spec =
         tensorstore::Spec::FromJson(
@@ -19,6 +35,30 @@ VczClass::VczClass(std::string t_vczFileName, std::vector<std::string> t_SampleI
                 {"kvstore", {{"driver", "file"}, {"path", path}}},
             })
             .value();
+    pimpl->m_store = tensorstore::Open<int8_t, tensorstore::dynamic_rank,
+                                       tensorstore::ReadWriteMode::read>(
+                         input_spec, tensorstore::OpenMode::open,
+                         tensorstore::ReadWriteMode::read)
+                         .value();
+    pimpl->m_chrom = "";
+    pimpl->m_pos = 1;
+    pimpl->m_startPos = 1;
+    pimpl->m_endPos = std::numeric_limits<int>::max();
+}
+
+void VczClass::set_iterator(std::string& chrom, const int start_pos,
+                            const int end_pos) {
+    pimpl->m_chrom = chrom;
+    pimpl->m_startPos = start_pos;
+    pimpl->m_endPos = end_pos;
+    pimpl->m_pos = start_pos;
+}
+
+void VczClass::move_forward_iterator(const int a) { pimpl->m_pos += a; }
+
+bool VczClass::check_iterator_end() {
+    // TODO
+    return true;
 }
 
 }  // namespace VCZ
--- src/VCZ.hpp
+++ src/VCZ.hpp
@@ -7,9 +7,16 @@
 namespace VCZ {
 
 class VczClass {
+   private:
+    class Impl;
+    std::unique_ptr<Impl> pimpl;
+
    public:
-    VczClass(std::string t_vczFileName, std::vector<std::string> t_SampleInModel);
-    bool setVczObj(const std::string &t_vczFileName);
+    VczClass(std::string t_vczFileName,
+             std::vector<std::string> t_SampleInModel);
+    void set_iterator(std::string& chrom, const int start_pos, const int end_pos);
+    void move_forward_iterator(int a);
+    bool check_iterator_end();
 };
 
 }  // namespace VCZ
